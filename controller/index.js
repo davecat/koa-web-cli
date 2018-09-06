fd2d5b1d55732b3981bc5b4fbcfa09bd13ca7825
davecat <2478496824@qq.com>
2018-09-06 15:12:37 +0800

    update

--- controller/index.js
+++ controller/index.js
@@ -5,14 +5,14 @@ module.exports = {
     index: async (ctx, next) => {
         //这里可以写业务逻辑，例如查数据库或者请求API
         //数据以对象{title: ''}形式传给模板
-        ctx.body = await ctx.render('index',{title:'index'});
+        ctx.body = await ctx.render('index',{title:'首页'});
     },
     viewer: async (ctx, next) => {
         // let res;
         // let tplData;
         // res = await request(config.url+'/apiUrl...');
         // tplData = JSON.parse(res.body);
-        ctx.body = await ctx.render('viewer',{title:'koa2',tplData: 'test'});
+        ctx.body = await ctx.render('viewer',{title:'Xtemplate',tplData: 'test'});
 
     }
 }
